953da57452bacf3a8bc973b074c7d0c0e8789481
Sansao77 <gabrielp_santos@hotmail.com>
2025-08-04 16:56:12 -0300

    first commit :tada:

---
+++ README.md
@@ -0,0 +1,9 @@
+# Softex (BFD) - Back End (Node)
+
+## 1º Aula (Introdução)
+
+    - Introduções e conhecimentos gerais
+    - Professor: Silvio Monte (Desenvolvedor Back End e Engenheiro de Redes)
+    - Instalação do Node
+    - Estudo sobre lógica de programação e variáveis
+    
---
+++ presencial/aula_1.js
@@ -0,0 +1,21 @@
+// Atividades Práticas
+
+// Atividade 1 - Guardando dados
+
+const cidade = "Recife", ano = 2025;
+
+console.log("Sou da cidade de " + cidade + " e estamos no ano de " + ano);
+
+// Atividade 2 - Calculando com variáveis
+
+const num1 = 5, num2 = 78;
+const soma = num1 + num2;
+
+console.log("\n" + soma);
+
+// Atividade 3 - Calculando o dobro
+
+let num = 50;
+num += num;
+
+console.log("\n" + num);
